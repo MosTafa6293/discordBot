dbb421407937b5565cbe07373a3420adef95b1c7
MosTafa6293 <mostafa.ahmed6293@gmail.com>
2022-06-20 12:57:28 +0200

    Discord Bot part2

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 const Discord = require("discord.js")
 require("dotenv").config()
 
+const generateImage = require("./generateImage")
+
 const TOKEN = process.env.TOKEN
 
-const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
+const client = new Discord.Client({
+    intents: [
+        "GUILDS",
+        "GUILD_MESSAGES",
+        "GUILD_MEMBERS"
+    ]
+});
 
 client.on("ready", () => {
     console.log(`logged in as ${client.user.tag}`)
@@ -13,6 +21,16 @@ client.on("messageCreate", (msg) => {
     if (msg.content == "hi") {
         msg.reply("Hello my friend!")
     }
+})
+
+const welcomeChannel = "988381639035998238"
+
+client.on("guildMemberAdd", async (member) => {
+    const image = await generateImage(member)
+    member.guild.channels.cache.get(welcomeChannel).send({
+        content: `Hey <@${member.id}>, Welcome to server!`,
+        files: [image]
+    })
 })
 
 client.login(TOKEN)
